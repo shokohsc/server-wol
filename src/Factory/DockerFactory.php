2acf9a1ea17e5953cc04c53fb700393e0cc259f4
shokohsc <shokohsc@gmail.com>
2020-01-06 23:59:53 +0100

    wip docker for wol

--- src/Factory/DockerFactory.php
+++ src/Factory/DockerFactory.php
@@ -12,7 +12,18 @@ class DockerFactory
      */
     public function build(): Docker
     {
-        $client = DockerClientFactory::createFromEnv();
+        $client = DockerClientFactory::create();
+        if (\getenv('DOCKER_TLS_VERIFY') && 'true' === \getenv('DOCKER_TLS_VERIFY')) {
+            $stream_context = [
+                'local_cert' => \getenv('DOCKER_CERT_PATH').DIRECTORY_SEPARATOR.'cert.pem',
+                'local_pk' => \getenv('DOCKER_CERT_PATH').DIRECTORY_SEPARATOR.'key.pem',
+            ];
+            $client = DockerClientFactory::create([
+                'remote_socket' => \getenv('DOCKER_HOST'),
+                'ssl' => true,
+                'stream_context_options' => $stream_context,
+            ]);
+        }
 
         return Docker::create($client);
     }
